850c0baccb9c26f2e9df3ff696a4b6f02d27d40b
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-14 19:52:14 -0300

    Agregue la peticion de login

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,5 +1,6 @@
 import { db } from "../database.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registrar = (req, res) => {
   const comprobar =
@@ -7,7 +8,7 @@ export const registrar = (req, res) => {
 
   db.query(comprobar, [req.body.nombreUsuario, req.body.email], (err, data) => {
     if (err) return res.status(500).json(err);
-    if (data.length) return res.json("El usuario ya existe");
+    if (data.length) return res.status(409).json("El usuario ya existe");
   });
 
   const salt = bcrypt.genSaltSync(10);
@@ -23,3 +24,24 @@ export const registrar = (req, res) => {
   });
 };
 
+export const login = (req, res) => {
+  const comprobar = "SELECT * FROM usuarios WHERE nombreUsuario = ?";
+  db.query(comprobar, [req.body.nombreUsuario], (err, data) =>{
+    if(err) return res.json(err);
+    if(data.length === 0) return res.status(404).json("Usuario no encontrado")
+    
+    const compContrasenia = bcrypt.compareSync(req.body.contrasenia, data[0].contrasenia);
+    
+    if(!compContrasenia) return res.status(400).json("La contraseña es incorrecta");
+    
+    const token = jwt.sign({id: data[0].id}, "jwtkey");
+    const {contrasenia, ...otros} = data[0];
+    
+    res.cookie("access_token", token, {
+      httpOnly:true
+    }).status(200).json(otros);
+  });
+  };
+  
+  export const logout = (req, res) => {};
+  
