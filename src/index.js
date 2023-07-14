1ea55326ecbb526d8e63d2f6c81adaa6b63081b1
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-14 19:17:49 -0300

    Agregue todas las rutas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import routerUsuarios from "./routes/usuarios.routes.js";
+import routerAuth from "./routes/auth.routes.js";
+import routerArticulos from "./routes/articulos.routes.js";
 
 const app = express();
 
@@ -9,3 +12,7 @@ app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("estoy en el puerto" + app.get("port"));
 });
+
+app.use("/auth", routerAuth);
+app.use("/usuarios", routerUsuarios);
+app.use("/articulos", routerArticulos);
