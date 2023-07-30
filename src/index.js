2a54b188d60e5d2d6770385cb9c038fa949604c1
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-30 12:44:15 -0300

    Agregue cors y cookieParser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import routerUsuarios from "./routes/usuarios.routes.js";
 import routerAuth from "./routes/auth.routes.js";
 import routerArticulos from "./routes/articulos.routes.js";
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 app.set("port", process.env.PORT || 4000);
 
