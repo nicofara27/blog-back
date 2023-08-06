cddda397417dd12bc8aef8f9f7c05d49b5369118
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-06 19:28:20 -0300

    Agregue funciones para agregar y borrar articulos

--- src/controllers/articulos.js
+++ src/controllers/articulos.js
@@ -1,4 +1,5 @@
 import { db } from "../database.js";
+import jwt from "jsonwebtoken";
 
 export const listarArticulos = (req, res) => {
   const listar = req.query.categoria
@@ -11,6 +12,7 @@ export const listarArticulos = (req, res) => {
     return res.status(200).json(data);
   });
 };
+
 export const traerArticulo = (req, res) => {
   const articulo =
     "SELECT a.id, `nombreUsuario`, `titulo`, `texto`, `img`, `categoria`,`fecha` FROM usuarios u JOIN articulos a ON u.id = a.uid WHERE a.id = ? ";
@@ -20,11 +22,51 @@ export const traerArticulo = (req, res) => {
     return res.status(200).json(data[0]);
   });
 };
+
 export const subirArticulo = (req, res) => {
-  res.json("ASD");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("No estas autenticado");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("El token no es valido");
+
+    const agregar =
+      "INSERT INTO articulos(`titulo`, `texto`, `img`, `categoria`, `fecha`, `uid`) VALUES (?)";
+
+    const values = [
+      req.body.titulo,
+      req.body.texto,
+      req.body.img,
+      req.body.categoria,
+      req.body.fecha,
+      userInfo.id,
+    ];
+
+    db.query(agregar, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+
+      return res.json("El articulo se agrego correctamente");
+    });
+  });
 };
+
 export const borrarArticulo = (req, res) => {
-  res.json("ASD");
+  const token = req.cookies.access_token;
+  console.log(req.cookies);
+  if (!token) return res.status(401).json("No estas autenticado");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("El token no es valido");
+
+    const artId = req.params.id;
+    const borrar = "DELETE FROM articulos WHERE id=? AND uid=?";
+
+    db.query(borrar, [artId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("Solo puedes borrar tus articulos");
+
+      return res.json("Artiuclo eliminado");
+    });
+  });
 };
 export const editarArticulo = (req, res) => {
   res.json("ASD");
