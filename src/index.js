8266de8232d450e70da9d66ca74f69fedee18019
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-07 19:29:34 -0300

    Borre una ruta que no utilizaba

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import routerUsuarios from "./routes/usuarios.routes.js";
-import routerAuth from "./routes/auth.routes.js";
 import routerArticulos from "./routes/articulos.routes.js";
 
 const app = express();
@@ -17,6 +16,5 @@ app.listen(app.get("port"), () => {
   console.log("estoy en el puerto" + app.get("port"));
 });
 
-app.use("/auth", routerAuth);
 app.use("/usuarios", routerUsuarios);
 app.use("/articulos", routerArticulos);
