4b6f6d51d61aef5a6d93cd9381eab0bd36a649d4
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-07 19:29:14 -0300

    Agregue las rutas y las validaciones para los articulos

---
+++ src/routes/articulos.routes.js
@@ -0,0 +1,62 @@
+import { Router } from "express";
+import {
+  borrarArticulo,
+  editarArticulo,
+  listarArticulos,
+  subirArticulo,
+  traerArticulo,
+} from "../controllers/articulos.js";
+import { check } from "express-validator";
+
+const router = Router();
+
+router.route("/").get(listarArticulos);
+router
+  .route("/:id")
+  .get(traerArticulo)
+  .put(
+    [
+      check("titulo")
+        .notEmpty()
+        .isLength({ min: 4, max: 70 })
+        .withMessage("El titulo debe tener entre 4 y 70 caracteres"),
+      check("texto").notEmpty(),
+      check("img")
+        .notEmpty()
+        .isLength({ min: 6, max: 100 })
+        .withMessage("La imagen debe tener entre 6 y 100 caracteres"),
+      check("categoria")
+        .notEmpty()
+        .isLength({ min: 4, max: 10 })
+        .withMessage("La categoria deve tener entre 4 y 10 caracteres"),
+      check("fecha").notEmpty(),
+      check("uid").notEmpty(),
+    ],
+    editarArticulo
+  )
+  .delete(borrarArticulo);
+
+router
+  .route("/subir")
+  .post(
+    [
+      check("titulo")
+        .notEmpty()
+        .isLength({ min: 4, max: 70 })
+        .withMessage("El titulo debe tener entre 4 y 70 caracteres"),
+      check("texto").notEmpty(),
+      check("img")
+        .notEmpty()
+        .isLength({ min: 6, max: 100 })
+        .withMessage("La imagen debe tener entre 6 y 100 caracteres"),
+      check("categoria")
+        .notEmpty()
+        .isLength({ min: 4, max: 10 })
+        .withMessage("La categoria deve tener entre 4 y 10 caracteres"),
+      check("fecha").notEmpty(),
+      check("uid").notEmpty(),
+    ],
+    subirArticulo
+  );
+
+export default router;
